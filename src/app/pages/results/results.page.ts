56bbd9d52d0d3c713228900f48c556109a2ad6fa
Nils Brödner <nilsbroedner@googlemail.com>
2025-12-28 23:06:33 +0100

    Integrate Sentry tracing for confetti animation in results page

--- src/app/pages/results/results.page.ts
+++ src/app/pages/results/results.page.ts
@@ -40,21 +40,24 @@ export class ResultsPage implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const duration = 15 * 1000;
-    const animationEnd = Date.now() + duration;
-    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+    Sentry.startSpan({ name: 'ResultsPage Confetti Animation' }, () => {
+      const duration = 15 * 1000;
+      const animationEnd = Date.now() + duration;
+      const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-    const interval = setInterval( () => {
-      const timeLeft = animationEnd - Date.now();
+      const interval = setInterval( () => {
+        const timeLeft = animationEnd - Date.now();
 
-      if (timeLeft <= 0) {
-        return clearInterval(interval);
-      }
+        if (timeLeft <= 0) {
+          return clearInterval(interval);
+        }
 
-      const particleCount = 50 * (timeLeft / duration);
-      // since particles fall down, start a bit higher than random
-      confetti({ ...defaults, particleCount, origin: { x: this.randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
-      confetti({ ...defaults, particleCount, origin: { x: this.randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
-    }, 250);
+        const particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti({ ...defaults, particleCount, origin: { x: this.randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+        confetti({ ...defaults, particleCount, origin: { x: this.randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+      }, 250);
+    });
+
   }
 }
